bbcfccd916aa15e77e434cd27bcc329cf06cbec4
elliefewings <erf33@cam.ac.uk>
2018-04-12 16:12:28 +0100

    Changes directory calling
    No longer requires explicit calling of local directory for easy running on other devices.

--- run.R
+++ run.R
@@ -1,3 +1,2 @@
 require(shiny)
-dir <- "C:\\Users\\ellie\\OneDrive - Clinical School Computing Service\\data_local\\GitHub\\ExAC_nonTCGA_explore_v7_run"
-shiny::runApp(appDir = dir, launch.browser=TRUE, port=3918)
+shiny::runApp(appDir = getwd(), launch.browser=TRUE, port=3918)
